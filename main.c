f6971e30b3eb6c54557d1801b3a4307262078565
MaxMaxM <kohotokk@mail.ru>
2018-09-18 19:19:56 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1;
+  int num2;
+  int sumnum;
+
+  char mychar;
+
+  num1 = 23;
+  num2 = 26;
+
+  printf("Hello World\n");
+  printf("Value of k = %d\n", num1);
+  printf("Value of to vars are: %d and %d\n", num1, num2);
+  sumnum = num1 + num2;
+  printf("Total is %d\n", sumnum);
+
+  mychar = 'd';
+  printf("mychar is %c\n", mychar);
+  printf("mychar is %d\n", mychar);
+
+
+  return 0;
+}
